b9b38d4ba560c9484a44631dadd06e1fb3c9341f
isaiah0311 <isaiahl0311@gmail.com>
2024-11-13 18:34:15 -1000

    Add creating random permutation

--- src/main.c
+++ src/main.c
@@ -7,6 +7,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+/**
+ * Swaps two integers.
+ * 
+ * \param[in] a First value.
+ * \param[in] b Second value.
+ */
+void swap(int* a, int* b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+/**
+ * Shuffles the values in an integer array.
+ * 
+ * \param[in] array Array to shuffle.
+ * \param[in] size Number of elements in the array.
+ */
+void shuffle(int* array, size_t size) {
+    for (size_t i = size - 1; i > 0; --i) {
+        size_t j = rand() % (i + 1);
+        swap(&array[i], &array[j]);
+    }
+}
 
 /**
  * Entry point for the program.
@@ -14,6 +40,33 @@
  * \return Exit code.
  */
 int main() {
-    printf("Noise Generator\n");
+    srand((unsigned) time(NULL));
+    
+    int perm[512] = { 0 };
+
+    for (int i = 0; i < 256; ++i) {
+        perm[i] = i;
+    }
+
+    shuffle(perm, 256);
+
+    for (int i = 256; i < 512; ++i) {
+        perm[i] = perm[i - 256];
+    }
+
+    for (int i = 0; i < 512; ++i) {
+        printf("%3d", perm[i]);
+
+        if (i < 511) {
+            printf(",");
+        }
+        
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        } else {
+            printf(" ");
+        }
+    }
+    
     return EXIT_SUCCESS;
 }
