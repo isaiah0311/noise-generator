45135447ba68228b1e14717fc5f4e86253d48e7a
isaiah0311 <isaiahl0311@gmail.com>
2024-11-13 17:10:39 -1000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * \file main.c
+ * \author Isaiah Lateer
+ * 
+ * Creates a bitmap of generated noise.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Entry point for the program.
+ * 
+ * \return Exit code.
+ */
+int main() {
+    printf("Noise Generator\n");
+    return EXIT_SUCCESS;
+}
